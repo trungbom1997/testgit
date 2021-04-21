0ddbc18c8c229685ea7c99a41fe42b48e7c4070f
trungbom <trungbom1997@gmail.com>
2021-04-21 10:05:14 +0700

    hhhh

--- python.py
+++ python.py
@@ -3,4 +3,4 @@ import requests,json
 response = requests.get('https://teechip.com/rest/retail-products/groups/587d0d41cee36fd012c64a69/shop/men/t-shirts?limit=36&useCatalogSearch=true&recentViewAsShould=true')
 data = response.text
 jsondata = json.loads(data)
-print ("sdfksjhfksdhfskjdhfks")
+print ("sdfksjhfksfsdfsdfsdfsdsdhfskjdhfks")
